0a0f62987eda691b3b389dd1b0ef263838f4b19c
Meghana N <meghananoochila@gmail.com>
2025-01-12 10:16:38 +0530

    Add files via upload

---
+++ returningsumsof1d.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int>sum(nums.size());
+        sum[0]=nums[0];
+        for(int i=1;i<nums.size();i++){
+            sum[i]=nums[i]+sum[i-1];
+        }
+        return sum;
+    }
+};
