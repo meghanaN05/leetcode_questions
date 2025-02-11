935ab3e09794294eff8714f0c5d5378215234163
Meghana N <meghananoochila@gmail.com>
2025-02-11 17:36:17 +0530

    Time: 3 ms (32.43%), Space: 9.5 MB (34.81%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,41 +1,9 @@
 class Solution {
 public:
-    bool check(stack<char>& st, string& part, int n) {
-        stack<char> tempst = st;
-        for (int idx = n - 1; idx >= 0; idx--) {
-            if (tempst.top() != part[idx]) {
-                return false;
-            }
-            tempst.pop();
-        }
-        return true;
-    }
-
     string removeOccurrences(string s, string part) {
-        int m = s.length();
-        int n = part.length();
-        stack<char> st;
-        
-    
-        for (int i = 0; i < m; i++) {
-            st.push(s[i]);
-
-           
-            if (st.size() >= n && check(st, part, n)) {
-                for (int j = 0; j < n; j++) {
-                    st.pop();
-                }
-            }
+        while (s.find(part) != string::npos) { 
+            s.erase(s.find(part), part.length());
         }
-
-       
-        string result = "";
-        while (!st.empty()) {
-            result.push_back(st.top());
-            st.pop();
-        }
-
-        reverse(result.begin(), result.end());
-        return result;
+        return s;
     }
 };
