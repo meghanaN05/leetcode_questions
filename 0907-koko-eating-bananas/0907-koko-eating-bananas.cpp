21874feacea010af191e9e1ca4a31b62f6e27691
Meghana N <meghananoochila@gmail.com>
2025-03-29 10:56:21 +0530

    Time: 7 ms (75.61%), Space: 22.9 MB (42.96%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,25 +1,29 @@
 class Solution {
-public:
-    bool checker(int mid, vector<int>& piles, int h) {
+
+    public:
+    bool checker(int mid, vector<int> &piles, int h) {
         long long hours = 0;
-        for (int pile : piles) {
-            hours += (pile + mid - 1) / mid;  
+        for(int i = 0; i < piles.size(); i++) {
+            hours += (ceil(piles[i] * 1.0 / mid));
         }
         return hours <= h;
     }
 
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int l = 1, r = *max_element(piles.begin(), piles.end()), ans = r;
-
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (checker(mid, piles, h)) {
+    int minEatingSpeed(vector<int> &piles, int h) {
+        int l = 1;
+        int r = INT_MIN;
+        for(int i = 0; i < piles.size(); i++) r = max(r, piles[i]);
+        int ans;
+        while(l <= r) {
+            int mid = (l + r) / 2;
+            if(checker(mid, piles, h)) {
                 ans = mid;
-                r = mid - 1;  
+                r = mid - 1;
             } else {
-                l = mid + 1;  
+                l = mid + 1;
             }
         }
         return ans;
     }
+
 };
