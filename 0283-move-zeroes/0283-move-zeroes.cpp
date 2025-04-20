33087aff716d1e61b91c6b3c77046f89ae3a7724
Meghana N <meghananoochila@gmail.com>
2025-04-20 17:57:59 +0530

    Time: 0 ms (100%), Space: 24.8 MB (5.75%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int zero=0;
+        vector<int>temp;
         for(int i=0;i<nums.size();i++){
-        if(nums[i]!=0) {
-            swap(nums[i],nums[zero]);
-            zero++;
+            if(nums[i]!=0)
+            temp.push_back(nums[i]);
         }
+        int nz=temp.size();
+        for(int i=0;i<nz;i++){
+            nums[i]=temp[i];
         }
-        
-        
-
+        for(int i=nz;i<nums.size();i++){
+            nums[i]=0;
+        }
     }
 };
