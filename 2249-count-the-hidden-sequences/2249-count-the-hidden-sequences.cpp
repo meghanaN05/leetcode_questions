988aa887baf206559ab014f7e4ff091511be52d6
Meghana N <meghananoochila@gmail.com>
2025-04-21 19:42:40 +0530

    Time: 0 ms (100%), Space: 112.6 MB (47.58%) - LeetHub

--- 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.cpp
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     int numberOfArrays(vector<int>& differences, int lower, int upper) {
-        long long min_val = 0, max_val = 0, curr = 0;
-        for (int diff : differences) {
-            curr += diff;
-            min_val = min(min_val, curr);
-            max_val = max(max_val, curr);
+        int curr = 0; //a[0] first element of hidden sequence
+        int minVal = 0;
+        int maxVal = 0;
+
+        for(int &d : differences) {
+            curr = curr + d;
+
+            minVal = min(minVal, curr);
+            maxVal = max(maxVal, curr);
+
+            if((upper - maxVal) - (lower - minVal) + 1 <= 0) {
+                return 0;
+            }
         }
-        long long min_start = lower - min_val;
-        long long max_start = upper - max_val;
-        long long ans = max_start - min_start + 1;
-        return ans > 0 ? ans : 0;
+
+        return (upper - maxVal) - (lower - minVal) + 1;
+
     }
 };
