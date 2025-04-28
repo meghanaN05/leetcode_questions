5ca1d590c086d0b99f6ba17f17b1ce51c6a40541
Meghana N <meghananoochila@gmail.com>
2025-04-28 22:56:23 +0530

    Time: 0 ms (100%), Space: 12.3 MB (38.99%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i=0; i<n; i++){
-            nums1[m+i] = nums2[i];
-        }
-        sort(nums1.begin(), nums1.end());
+        int mid = m - 1;
+        int nid = n - 1;
+        int r = m + n - 1;
+
+        while (nid>= 0) {
+            if (mid >= 0 && nums1[mid] > nums2[nid]) {
+                nums1[r] = nums1[mid];
+                mid--;
+            } else {
+                nums1[r] = nums2[nid];
+                nid--;
+            }
+            r--;
+        }        
     }
 };
