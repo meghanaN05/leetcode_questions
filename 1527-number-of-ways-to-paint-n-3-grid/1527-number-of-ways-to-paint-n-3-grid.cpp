64b3d44d6f101b960d737fe7033daefb8610c0db
Meghana N <meghananoochila@gmail.com>
2025-05-18 22:35:52 +0530

    Time: 14 ms (37.69%), Space: 14.6 MB (20.39%) - LeetHub

---
+++ 1527-number-of-ways-to-paint-n-3-grid/1527-number-of-ways-to-paint-n-3-grid.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int mod = 1e9 + 7;
+
+    int numOfWays(int n) {
+        vector<vector<long long>> dp(n, vector<long long>(3, 0));
+        dp[0][1] = dp[0][2] = 6;
+        for(int i = 1; i < n; i++) {
+            dp[i][2] = ((dp[i - 1][1] * 2LL) % mod + (dp[i - 1][2] * 2LL) % mod) % mod;
+            dp[i][1] = ((dp[i - 1][1] * 3LL) % mod + (dp[i - 1][2] * 2LL) % mod) % mod;
+        }
+        return (dp[n - 1][1] + dp[n - 1][2]) % mod;
+    }
+};
