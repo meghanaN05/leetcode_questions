75f062c50267f237ca2b086f7805411a0f135fda
Meghana N <meghananoochila@gmail.com>
2025-05-19 19:00:11 +0530

    Time: 0 ms (100%), Space: 8.1 MB (25.35%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -3,9 +3,14 @@ public:
     int minBitFlips(int start, int goal) {
         int ans=start^goal;
         int cnt=0;
-        for(int i=0;i<31;i++){
-            if(ans&(1<<i))
-            cnt++;
+        // for(int i=0;i<31;i++){
+        //     if(ans&(1<<i))
+        //     cnt++;
+        // }
+        while(ans!=0){
+       
+         if(ans%2==1) cnt++;
+          ans=ans/2;
         }
         return cnt;
     }
