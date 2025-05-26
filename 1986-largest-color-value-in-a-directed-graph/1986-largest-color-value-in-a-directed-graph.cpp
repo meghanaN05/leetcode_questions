c8cf00c3e6f11fada4919ebfffbbebfbc60275a1
Meghana N <meghananoochila@gmail.com>
2025-05-26 23:27:20 +0530

    Time: 295 ms (98.33%), Space: 147.9 MB (97.77%) - LeetHub

--- 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.cpp
+++ 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.cpp
@@ -1,29 +1,61 @@
 class Solution {
-public:
-    int dfs(int node, string &colors, vector<vector<int>> &adj, vector<vector<int>> &count, vector<int> &vis) {
-        if (!vis[node]) {
-            vis[node] = 1;
-            for (int next : adj[node]) {
-                if (dfs(next, colors, adj, count, vis) == INT_MAX)
-                    return INT_MAX;
-                for (int c = 0; c < 26; c++)
-                    count[node][c] = max(count[node][c], count[next][c]);
-            }
-            ++count[node][colors[node] - 'a'];
-            vis[node] = 2;
-        }
-        return vis[node] == 2 ? count[node][colors[node] - 'a'] : INT_MAX;
-    }
+	int colors[26] = {0};
+	vector<vector<int>> graph;
+	vector<int> indegree;
+	vector<array<int, 26>> colorMapping;
+	int numElements;
 
-    int largestPathValue(string colors, vector<vector<int>> &edges) {
-        int n = colors.size();
-        vector<vector<int>> adj(n), count(n, vector<int>(26));
-        vector<int> vis(n);
-        for (auto &e : edges)
-            adj[e[0]].push_back(e[1]);
-        int ans = 0;
-        for (int i = 0; i < n && ans != INT_MAX; i++)
-            ans = max(ans, dfs(i, colors, adj, count, vis));
-        return ans == INT_MAX ? -1 : ans;
-    }
+	void edgesToGraph(vector<vector<int>>& edges) {
+		graph.assign(numElements, vector<int>{});
+		indegree.assign(numElements, 0);
+
+		for (vector<int>& edge : edges) {
+			graph[edge[0]].push_back(edge[1]);
+			indegree[edge[1]]++;
+		}
+	}
+public:
+	int largestPathValue(string colors, vector<vector<int>>& edges) {
+		numElements = colors.size();
+		colorMapping.assign(numElements, array<int, 26>{});
+		edgesToGraph(edges);
+
+		queue<int> q;
+		for (int i = 0; i < numElements; i++) {
+			if (!indegree[i]) q.push(i);
+		}
+
+		int temp;
+		int maxColor = 0;
+		while (!q.empty()) {
+			temp = q.front();
+			q.pop();
+			colorMapping[temp][colors[temp] - 'a']++;
+			for (int i = 0; i < 26; i++) {
+				if (!colorMapping[temp][i]) continue;
+				maxColor = max(maxColor, colorMapping[temp][i]);
+			}	
+
+			for (int nbr : graph[temp]) {
+				if (indegree[nbr]) {
+					indegree[nbr]--;
+					for (int i = 0; i < 26; i++) {
+						if (!colorMapping[temp][i]) continue;
+						colorMapping[nbr][i] = max(colorMapping[nbr][i], colorMapping[temp][i]);
+					}
+					if (!indegree[nbr]) {
+						q.push(nbr);					
+					}
+				}
+			}
+		}
+
+        for (int i = 0; i < numElements; i++) {
+            if (indegree[i]) return -1;
+        }
+
+
+		return maxColor;
+
+	}
 };
