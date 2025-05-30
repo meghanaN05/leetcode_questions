d5b09c3fd018206d2ad2efcd8fbed5045d813914
Meghana N <meghananoochila@gmail.com>
2025-05-30 09:03:32 +0530

    Time: 0 ms (100%), Space: 7.8 MB (95.38%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -1,26 +1,10 @@
 class Solution {
 public:
-    string result = "";
-
- 
-    void helper(int k, string current) {
-        if (current.size() >= k) {
-            result = current;
-            return;
-        }
-
-        string nextPart = "";
-        for (char c : current) {
-           
-            char nextChar = (c == 'z') ? 'a' : c + 1;
-            nextPart += nextChar;
-        }
-
-        helper(k, current + nextPart);
-    }
-
     char kthCharacter(int k) {
-        helper(k, "a");
-        return result[k - 1]; 
-    }
+  
+    int flips = __builtin_popcount(k-1);
+
+    return (char)('a' + flips);
+}
+
 };
