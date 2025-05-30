7db221c33d976f893daf9ae22d2cd94f63e3a735
Meghana N <meghananoochila@gmail.com>
2025-05-30 09:11:36 +0530

    Time: 4 ms (47.22%), Space: 8.9 MB (77.73%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    bool helper(int n) {
-        if (n == 1) return true;
-        if (n <= 0 || n % 3 != 0) return false;
-        return helper(n / 3);
-    }
-
     bool isPowerOfThree(int n) {
-        return helper(n);
+        if(n <= 0) return false;
+
+        while(n % 3 == 0)
+        {
+            n /= 3;
+        }
+        return n == 1;
+        
     }
 };
