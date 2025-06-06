aeb82e2f33b312974d6af2e8cc7f05513ffbaffe
Meghana N <meghananoochila@gmail.com>
2025-06-06 19:10:49 +0530

    Time: 15 ms (18.24%), Space: 11.6 MB (9.81%) - LeetHub

--- 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.cpp
+++ 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.cpp
@@ -1,51 +1,32 @@
 class Solution {
 public:
-    vector<int> par, size;
-    int findParent(int u){
-        if(par[u] == u) return u;
-
-        return par[u] = findParent(par[u]);
+char DFSarray(unordered_map<int,vector<char>>&a,char &ch,vector<int>&visited){
+    visited[ch-'a']=1;
+    char minchar=ch;
+    for(char &v:a[ch]){
+        if(visited[v-'a']==0){
+            minchar=min(minchar,DFSarray(a,v,visited));
+        }
     }
-    void Union(int u, int v){
-        int up = findParent(u);
-        int vp = findParent(v);
-
-        if(up == vp) return;
-        if(size[up] > size[vp]){
-            size[up]+=size[vp];
-            par[vp] = up;
-        }
-        else{
-            size[vp]+=size[up];
-            par[up] = vp;
-        }
+    return minchar;
     }
     string smallestEquivalentString(string s1, string s2, string baseStr) {
-        par.resize(26, 0);
-        for(int i=0; i<26; i++) par[i] = i;
-        size.resize(26, 0);
-        for(int i=0; i<s1.size(); i++){
-            int c1 = s1[i]-'a';
-            int c2 = s2[i]-'a';
-
-            Union(c1, c2);
+        int n=s1.size();
+        int m=baseStr.size();
+        unordered_map<int,vector<char>>a;
+        for(int i=0;i<n;i++){
+            char u=s1[i];
+            char v=s2[i];
+            a[u].push_back(v);
+             a[v].push_back(u);
         }
-
-        // for(int i=0; i<par.size(); i++) cout<<size[i]<< " ";
-
-        map<int, int> mp;
-        for(int i=0; i<par.size(); i++){
-            int p = findParent(i);
-            if(mp.find(p) == mp.end())
-                mp[p] = i;
-            else mp[p] = min(mp[p], i);
+        string result;
+        for(int i=0;i<m;i++){
+            char ch=baseStr[i];
+            vector<int>visited(26,0);
+            char minchar=DFSarray(a,ch,visited);
+            result.push_back(minchar);
         }
-
-        string ans;
-        for(auto c: baseStr){
-            int x = findParent(c-'a');
-            ans.push_back(mp[x]+'a');
-        }
-        return ans;
+        return result;
     }
 };
