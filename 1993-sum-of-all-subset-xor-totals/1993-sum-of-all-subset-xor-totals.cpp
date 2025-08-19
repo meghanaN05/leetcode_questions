97cd3e7ed8f707679830dc2c29d2813bef6170e0
Meghana N <meghananoochila@gmail.com>
2025-08-19 10:05:10 +0530

    Time: 0 ms (100%), Space: 9.2 MB (37.79%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     int subsetXORSum(vector<int>& nums) {
-        return dfs(nums, 0, 0);
-    }
-    
-    int dfs(vector<int>& nums, int i, int curXor) {
-        if (i == nums.size()) return curXor;
-     
-        return dfs(nums, i + 1, curXor ^ nums[i]) + dfs(nums, i + 1, curXor);
+        int orAll = 0;
+        for (int x : nums) orAll |= x;
+        return orAll * (1 << (nums.size() - 1));
     }
 };
