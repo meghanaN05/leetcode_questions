05a3b16a15d99100cc62107a1b68c5a2182d9472
Meghana N <meghananoochila@gmail.com>
2025-08-22 23:13:08 +0530

    Time: 0 ms (100%), Space: 11.5 MB (28.39%) - LeetHub

---
+++ 1137-height-checker/1137-height-checker.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int>expected=heights;
+        int ans=0;
+        sort(expected.begin(),expected.end());
+        for(int i=0;i<heights.size();i++){
+            if(heights[i]!=expected[i])
+            ans++;
+        }
+        return ans;
+    }
+};
