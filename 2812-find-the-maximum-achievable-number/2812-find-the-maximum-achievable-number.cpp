54f5e3e319783ad3d795514e713d01096a9cceb4
Meghana N <meghananoochila@gmail.com>
2025-08-28 10:44:25 +0530

    Time: 3 ms (7%), Space: 9.7 MB (6.83%) - LeetHub

---
+++ 2812-find-the-maximum-achievable-number/2812-find-the-maximum-achievable-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        int ans=num;
+        for(int i=0;i<t;i++){
+            ans+=2;
+        }
+        return ans;
+    }
+};
