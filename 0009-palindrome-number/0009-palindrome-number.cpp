7f993d8b27124f8676d4801d0abcb47f7efa65f7
Meghana N <meghananoochila@gmail.com>
2025-09-05 12:29:33 +0530

    Update 0009-palindrome-number.cpp

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // Negative numbers cannot be palindromes
+      
         if (x < 0) {
             return false;
         }
 
-        int original = x;  // Store the original value of x
-        long long sum = 0;       // Reversed number
+        int original = x;  
+        long long sum = 0;       
 
-        // Reverse the digits of x
+    
         while (x != 0) {
             int remainder = x % 10;
             sum = 10 * sum + remainder;
             x = x / 10;
         }
 
-        // Compare reversed number with the original
+
         return sum == original;
     }
 };
