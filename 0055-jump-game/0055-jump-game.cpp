d4c1819823f869ecce265e34b99b90bfb5066823
Meghana N <meghananoochila@gmail.com>
2025-09-24 10:43:26 +0530

    Time: 0 ms (100%), Space: 52.3 MB (66.32%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int curr=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(i>curr) return false;
+            curr=max(curr,i+nums[i]);
+        }
+        return true;
+    }
+};
