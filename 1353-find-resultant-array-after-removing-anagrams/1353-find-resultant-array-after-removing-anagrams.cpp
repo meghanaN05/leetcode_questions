7c90db7950bc7c803af923c1190794ddf49735ed
Meghana N <meghananoochila@gmail.com>
2025-10-13 18:31:39 +0530

    Time: 0 ms (100%), Space: 15.9 MB (96.97%) - LeetHub

---
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> removeAnagrams(vector<string>& words) {
+        vector<string> result;
+        string prevSorted = "";
+        for (string word : words) {
+            string sortedWord = word;
+            sort(sortedWord.begin(), sortedWord.end());
+            if (sortedWord != prevSorted) {
+                result.push_back(word);
+                prevSorted = sortedWord;
+            }
+        }
+        return result;
+    }
+};
