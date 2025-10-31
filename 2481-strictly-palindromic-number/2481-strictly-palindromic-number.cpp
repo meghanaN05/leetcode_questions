bbd13d70d64d0e692c425145f0b5f9ebfeb12c40
Meghana N <meghananoochila@gmail.com>
2025-10-31 10:40:23 +0530

    Time: 0 ms (100%), Space: 8.2 MB (3.62%) - LeetHub

--- 2481-strictly-palindromic-number/2481-strictly-palindromic-number.cpp
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
+    string toBase(int n, int base) {
+        string res = "";
+        while (n > 0) {
+            res += to_string(n % base);
+            n /= base;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+
     bool isStrictlyPalindromic(int n) {
-        return false;
+        for (int base = 2; base <= n - 2; base++) {
+            string s = toBase(n, base);
+            string rev = s;
+            reverse(rev.begin(), rev.end());
+            if (s != rev) return false;
+        }
+        return true;
     }
 };
