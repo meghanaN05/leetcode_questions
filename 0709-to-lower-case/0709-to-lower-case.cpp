4fc9714db46880878a4b0e5c52cdc0e36591c59b
Meghana N <meghananoochila@gmail.com>
2025-12-04 23:54:45 +0530

    Time: 0 ms (100%), Space: 8.4 MB (1.7%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+         string result="";
+         for(int i=0;i<s.size();i++){
+            result+=tolower(s[i]);
+         }
+         return result;
+    }
+};
