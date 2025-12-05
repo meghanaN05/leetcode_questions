dab59604c056a1334ae8f4486488c09959db8f51
Meghana N <meghananoochila@gmail.com>
2025-12-05 10:10:35 +0530

    Time: 0 ms (100%), Space: 22.4 MB (53.79%) - LeetHub

---
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int countPartitions(vector<int>& nums) {
+        int total = accumulate(nums.begin(), nums.end(), 0);
+          return total%2==0?nums.size()-1:0;
+    }
+};
+
