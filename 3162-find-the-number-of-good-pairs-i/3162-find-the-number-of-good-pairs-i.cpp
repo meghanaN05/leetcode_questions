36c97571c435a62740f8198025a65be8fe4ff583
Meghana N <meghananoochila@gmail.com>
2025-12-10 21:53:10 +0530

    Time: 0 ms (100%), Space: 42.4 MB (7.64%) - LeetHub

---
+++ 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        for(int i=0;i<nums2.size();i++){
+            nums2[i]=k*nums2[i];
+        }
+        int cnt=0;
+        for(int i=0;i<nums1.size();i++){
+            for(int j=0;j<nums2.size();j++){
+           if(nums2[j]!=0) {
+           if(nums1[i]%nums2[j]==0) cnt++;
+           } 
+            }
+        }
+        return cnt;
+    }
+};
