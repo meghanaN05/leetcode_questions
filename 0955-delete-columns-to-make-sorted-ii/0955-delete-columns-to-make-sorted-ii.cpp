bfb80e0039665fbe5ce9f10d13554c77690c35e6
Meghana N <meghananoochila@gmail.com>
2025-12-21 23:02:56 +0530

    Time: 0 ms (100%), Space: 12.8 MB (62.92%) - LeetHub

---
+++ 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int r=strs.size();
+        int c=strs[0].size();
+        int d=0;
+        vector<int>srt(r,false);
+        int i=0;
+        int j=0;
+        for(int j=0;j<c;j++){
+            bool deleted=0;
+            for(int i=0;i<r-1;i++){
+                if(!srt[i] && strs[i][j]>strs[i+1][j]){
+                    d++;
+                    deleted=true;
+                    break;
+                }
+            }
+            if(deleted){
+                continue;
+            }
+            for(int i=0;i<r-1;i++){
+                srt[i]=srt[i]|(strs[i][j]<strs[i+1][j]);
+            }
+        }
+        return d;
+    }
+};
