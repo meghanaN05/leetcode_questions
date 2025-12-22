b1ab4d2074f8c7d769f86177489baf9c55ef5a0a
Meghana N <meghananoochila@gmail.com>
2025-12-22 19:52:30 +0530

    Time: 3 ms (30.66%), Space: 13.9 MB (83.27%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int>ans;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]){
+                i++;
+            }
+            else{
+                ans.push_back(nums[i]);
+            }
+        }
+        if(nums[nums.size()-1]!=nums[nums.size()-2])
+        ans.push_back(nums[nums.size()-1]);
+        return ans;
+    }
+};
