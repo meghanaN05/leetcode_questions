67263c6216bdb9974b652658f328d531cf868f39
Meghana N <meghananoochila@gmail.com>
2025-12-27 09:34:18 +0530

    Time: 0 ms (100%), Space: 17.1 MB (65.9%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,15 +1,31 @@
 class Solution {
-public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        priority_queue<int>pq;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                pq.push(matrix[i][j]);
-                if(pq.size()>k) pq.pop();
+  public:
+  int clk(vector<vector<int>> &mat, int mid,int n,int m){
+      int cnt=0,c=m-1;
+      for(int i=0;i<n;i++){
+          while(c>=0 && mat[i][c]>mid) c--;
+          cnt+=(c+1);
+      }
+      return cnt;
+  }
+    int kthSmallest(vector<vector<int>> &mat, int k) {
+        int n=mat.size();
+        int m=mat[0].size();
+        int l=mat[0][0];
+        int r=mat[n-1][m-1];
+        int ans=0;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(clk(mat,mid,n,m)>=k){
+                ans=mid;
+                r=mid-1;
             }
+            else{
+               l=mid+1;
+            }
+            
         }
-        return pq.top();
+        return ans;
+        
     }
 };
