bea8396df67cf18bb924a7a85511fa754b76210c
Meghana N <meghananoochila@gmail.com>
2026-01-01 11:48:18 +0530

    Time: 0 ms (100%), Space: 11.6 MB (50.17%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
-        
-       
         for (int i = n - 1; i >= 0; --i) {
             if (digits[i] < 9) {
                 digits[i]++; 
@@ -11,8 +9,6 @@ public:
             }
             digits[i] = 0;  
         }
-
-        
         digits.insert(digits.begin(), 1);
         return digits;
     }
