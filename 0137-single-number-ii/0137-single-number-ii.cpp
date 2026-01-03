9116459bc3df2df3c6caa87852b946553753e9f2
Meghana N <meghananoochila@gmail.com>
2026-01-03 17:37:54 +0530

    Time: 0 ms (100%), Space: 13.1 MB (98.32%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result=0;
+       for(int i=0;i<32;i++){
+        int cnt=0;
+        for(int j=0;j<nums.size();j++){
+            if((nums[j]>>i)&1)
+             cnt++;
+        }
+        if(cnt%3!=0){
+            result|=(1<<i);
+        }
+       }
+       return result;
+    }
+};
