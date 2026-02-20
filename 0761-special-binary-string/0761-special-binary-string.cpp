5df2b0f1eb43ce6893473ee798e02cda2c61b1b3
Meghana N <meghananoochila@gmail.com>
2026-02-20 23:58:15 +0530

    Time: 0 ms (100%), Space: 9.7 MB (21.2%) - LeetHub

---
+++ 0761-special-binary-string/0761-special-binary-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string makeLargestSpecial(string s) {
+        int cnt = 0;
+        int i = 0;
+        vector<string> res;   
+        for (int j = 0; j < s.length(); j++) {
+            cnt += (s[j] == '1') ? 1 : -1;
+            if (cnt == 0) {
+                res.push_back("1" + makeLargestSpecial(s.substr(i + 1, j - i - 1)) + "0");
+                i = j + 1;
+            }
+        }
+        sort(res.rbegin(), res.rend());
+        string ans;
+        for (const string &str : res) {
+            ans += str;
+        }
+
+        return ans;
+    }
+};
