9fd1187ea44326ae8180d0cd9e01ed3182108f58
Meghana N <meghananoochila@gmail.com>
2026-03-03 12:09:43 +0530

    Time: 0 ms (100%), Space: 18 MB (21.3%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
-         int n=nums.size();
-        int l=0;
-        int r=n-1;
+    bool search(vector<int>& arr, int key) {
+          int l=0;
+        int r=arr.size()-1;
         while(l<=r){
             int mid=l+(r-l)/2;
-            if(nums[mid]==target) return true;
-            if(nums[l]==nums[mid] && nums[mid]==nums[r]){
+            if(arr[mid]==key) return true;
+            if(arr[l]==arr[mid] && arr[mid]==arr[r]){
                 l++;
                 r--;
                 continue;
             }
-            if(nums[l]<=nums[mid]){
-             if(nums[l]<=target && target<=nums[mid]){
-                r=mid-1;
-             }
-             else{
-                l=mid+1;
-             }
+            if(arr[l]<=arr[mid]){
+                if(arr[l]<=key && key<=arr[mid]){
+                    r=mid-1;
+                }
+                else{
+                    l=mid+1;
+                }
+                
             }
             else{
-             if(nums[mid]<=target && target<=nums[r]){
-                l=mid+1;
-             }
-             else{
-                r=mid-1;
-             }
+                  if(key>=arr[mid] && key<=arr[r]){
+                    l=mid+1;
+                }
+                else{
+                   r=mid-1;
+                }
             }
         }
         return false;
