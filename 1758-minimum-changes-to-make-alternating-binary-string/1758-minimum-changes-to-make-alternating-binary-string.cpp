90b1c6afbd9022ea8c6dde339e58e3a5cc84b882
Meghana N <meghananoochila@gmail.com>
2026-03-05 10:23:20 +0530

    Time: 0 ms (100%), Space: 9.5 MB (20.96%) - LeetHub

---
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minOperations(string s) {
+      int cnt = 0;
+     for(int i=0;i<s.size();i++){
+    if((s[i]-'0') != (i%2))
+        cnt++;
+}
+return min(cnt, (int)s.size()-cnt);
+    }
+};
