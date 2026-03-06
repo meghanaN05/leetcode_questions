850f22c6964a9d20186d1c312fa1bb6fe1a6d5ac
Meghana N <meghananoochila@gmail.com>
2026-03-06 16:40:54 +0530

    Time: 7 ms (30.6%), Space: 64.5 MB (92.24%) - LeetHub

--- 2643-row-with-maximum-ones/2643-row-with-maximum-ones.cpp
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
+//binary search approach
     vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+        int maxi=0;
         int row=0;
-         int maxi=0;
+        int m=mat[0].size();
      for(int i=0;i<mat.size();i++){
-        int cnt=0;
-        for(int j=0;j<mat[0].size();j++){
-            if(mat[i][j]==1) cnt++;
-        }
-        if(cnt>maxi){
-           maxi=cnt;
-           row=i;
-        }
-     }
-     vector<int>ans(2);
-     ans[0]=row;
-     ans[1]=maxi;
+        sort(mat[i].begin(),mat[i].end());
+      int l=0,r=mat[0].size()-1;
+      int ans=m;
+      int cnt=0;
+      while(l<=r){
+       int mid=l+(r-l)/2;
+       if(mat[i][mid]==1){
+        ans=mid;
+        r=mid-1;
+       }
+       else{
+        l=mid+1;
+       }
+      }
+       cnt=m-ans;
+       if(cnt>maxi){
+        maxi=cnt;
+        row=i;
+       }
+      }
+     
+      vector<int>ans(2);
+      ans[0]=row;
+      ans[1]=maxi;
      return ans;
     }
 };
