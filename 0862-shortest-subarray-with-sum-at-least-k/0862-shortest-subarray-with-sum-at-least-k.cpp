4d6fd3a55608e18f8f6bdaaee4ba53256e3a269c
Meghana N <meghananoochila@gmail.com>
2026-03-10 09:21:32 +0530

    Time: 16 ms (84.43%), Space: 109.3 MB (42.91%) - LeetHub

---
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int shortestSubarray(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<long long>prefix(n+1,0);
+        deque<int>dq;
+        for(int i=0;i<n;i++){
+            prefix[i+1]=nums[i]+prefix[i];
+        }
+        int res=n+1;
+        for(int i=0;i<=n;i++){
+        while(!dq.empty() && prefix[i]-prefix[dq.front()]>=k){
+            res=min(res,i-dq.front());
+            dq.pop_front();
+        }
+         while(!dq.empty() && prefix[i]<=prefix[dq.back()]){
+            dq.pop_back();
+        }
+        dq.push_back(i);
+        }
+        return ((res==n+1)?-1:res);
+
+    }
+};
