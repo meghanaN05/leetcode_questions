b863c606c68c2500cc7d4e306f449ad6bfebe37d
Meghana N <meghananoochila@gmail.com>
2026-03-11 17:48:38 +0530

    Time: 0 ms (100%), Space: 8 MB (45.49%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int findComplement(int num) {
-        string s = bitset<32>(num).to_string();
-        s.erase(0, s.find_first_not_of('0'));
-        for (int i = 0; i < s.size(); i++) {
-            s[i] = (s[i] == '0' ? '1' : '0');
+        if(num==0) return 1;
+        int temp=num;
+        int mask=0;
+        while(temp){
+            mask=(mask<<1)|1;
+            temp=temp>>1;
         }
-        return stoi(s, nullptr, 2); 
+        return mask^num;
     }
 };
