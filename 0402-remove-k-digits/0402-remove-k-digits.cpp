0f425413440e586dc4dce9632c1f47d58cdc5911
Meghana N <meghananoochila@gmail.com>
2026-03-13 19:46:08 +0530

    Time: 2 ms (69.87%), Space: 11.8 MB (31.47%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        stack<char> st;
+        for(int i = 0; i < num.size(); i++) {
+            while(!st.empty() && k > 0 && st.top() > num[i]) {
+                st.pop();
+                k--;
+            }
+            st.push(num[i]);
+        }
+        while(k > 0 && !st.empty()) {
+            st.pop();
+            k--;
+        }
+        string ans = "";
+        while(!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        int i = 0;
+        while(i < ans.size() && ans[i] == '0') i++;
+        ans = ans.substr(i);
+        return ans == "" ? "0" : ans;
+    }
+};
