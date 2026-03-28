0cabdade25f49e0c82ab0d039faf5b9f906a7493
Meghana N <meghananoochila@gmail.com>
2026-03-28 19:50:45 +0530

    Time: 7 ms (18.72%), Space: 18.1 MB (8.42%) - LeetHub

---
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        int cnt=0;
+        for(int i=0;i<nums.size();i++){
+            cnt+=mp[nums[i]-k];
+            cnt+=mp[nums[i]+k];
+            mp[nums[i]]++;
+        }
+        return cnt;
+    }
+};
