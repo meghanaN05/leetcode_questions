bd5be089907dbc95b5dff93005d5d5e84ce5f0c7
Meghana N <meghananoochila@gmail.com>
2026-04-02 00:18:35 +0530

    Time: 62 ms (42.52%), Space: 195.2 MB (43.71%) - LeetHub

---
+++ 2751-robot-collisions/2751-robot-collisions.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        int n = positions.size();
+        vector<int> order(n);
+        iota(order.begin(), order.end(), 0);
+        sort(order.begin(), order.end(),
+             [&](int a,int b){ return positions[a] < positions[b]; });
+        vector<bool> alive(n,true);
+        vector<int> st;
+        for(int idx:order){
+            if(directions[idx]=='R') st.push_back(idx);
+            else{
+                while(!st.empty()){
+                    int top = st.back();
+                    if(healths[top] < healths[idx]){
+                        alive[top]=false;
+                        st.pop_back();
+                        healths[idx]--;
+                    }
+                    else if(healths[top] > healths[idx]){
+                        alive[idx]=false;
+                        healths[top]--;
+                        break;
+                    }
+                    else{
+                        alive[top]=false;
+                        alive[idx]=false;
+                        st.pop_back();
+                        break;
+                    }
+                }
+            }
+        }
+        vector<int> res;
+        for(int i=0;i<n;i++)
+            if(alive[i]) res.push_back(healths[i]);
+
+        return res;
+    }
+};
