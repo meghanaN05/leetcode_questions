afd9b663afa36edb7a6ac363141e590fb89bcb61
Meghana N <meghananoochila@gmail.com>
2026-04-29 23:46:45 +0530

    Time: 0 ms (100%), Space: 23.1 MB (53.98%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -9,19 +9,19 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
- int height(TreeNode* root){
-    if(root==NULL) return 0;
-    int lh=height(root->left);
-    if(lh==-1) return -1;
-     int rh=height(root->right);
-    if(rh==-1) return -1;
-    if(abs(lh-rh)>1) return -1;
-    return 1+max(rh,lh);
-    
- } 
 class Solution {
 public:
+    int isb(TreeNode* root){
+        if(root==NULL) return 0;
+        int l=isb(root->left);
+        if(l==-1) return -1;
+          int r=isb(root->right);
+        if(r==-1) return -1;
+        if(abs(l-r)>1) return -1;
+        return 1+max(l,r);
+    }
     bool isBalanced(TreeNode* root) {
-        return height(root)!=-1;
+       if(root==NULL) return true;
+       return isb(root)!=-1;
     }
 };
