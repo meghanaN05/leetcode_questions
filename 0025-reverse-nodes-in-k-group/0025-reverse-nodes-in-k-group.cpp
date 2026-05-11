15ff22686108a794b33586f0773d140282d6b2e3
Meghana N <meghananoochila@gmail.com>
2026-05-11 22:12:24 +0530

    Time: 0 ms (100%), Space: 16.5 MB (69.14%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -1,28 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
+    ListNode* rev(ListNode* head){
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+        while(temp){
+            ListNode* dummy = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = dummy;
+        }
+        return prev;
+    }
+
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if (!head || k == 1) return head;
         int cnt = 0;
         ListNode* temp = head;
-        while (temp) {
+        while(temp){
             cnt++;
             temp = temp->next;
         }
         ListNode dummy(0);
         dummy.next = head;
         ListNode* prev = &dummy;
-        ListNode* curr;
-        ListNode* nex;
-        while (cnt >= k) {
-            curr = prev->next;
-            nex = curr->next;
-            for (int i = 1; i < k; i++) {
-                curr->next = nex->next;
-                nex->next = prev->next;
-                prev->next = nex;
-                nex = curr->next;
+        while(cnt >= k){
+            ListNode* start = prev->next;
+            ListNode* end = start;
+            for(int i = 1; i < k; i++){
+                end = end->next;
             }
-            prev = curr;
+            ListNode* nextgroup = end->next;
+            end->next = NULL;
+            ListNode* reversedHead = rev(start);
+            prev->next = reversedHead;
+            start->next = nextgroup;
+            prev = start;
             cnt -= k;
         }
         return dummy.next;
