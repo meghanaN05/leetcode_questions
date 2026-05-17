b1c9d1a448a82be4e37bde88796986387dac068a
Meghana N <meghananoochila@gmail.com>
2026-05-17 09:30:31 +0530

    Time: 4 ms (32.12%), Space: 38.5 MB (47.62%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+     int n;
+       bool dfs(vector<int>& arr, int i){
+        n=arr.size();
+        if(i<0 || i>=n || arr[i]<0) return false;
+        if(arr[i]==0) return true;
+        arr[i]*=-1;
+        int l=dfs(arr,i-arr[i]);
+        int r=dfs(arr,i+arr[i]);
+        return l||r;
+       }
+
+    bool canReach(vector<int>& arr, int start) {
+       return dfs(arr,start);
+    }
+};
