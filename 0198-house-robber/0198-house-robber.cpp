6ae31c6b35c3ac8b4221050a1c5eee0b5d9e8347
Meghana N <meghananoochila@gmail.com>
2026-05-17 10:43:42 +0530

    Time: 0 ms (100%), Space: 10.8 MB (20.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-//memorization code
-   int solve(int i,vector<int>&memo,vector<int>& nums,int n){
-    if(i>=n) return 0;
-    if(memo[i]!=-1) return memo[i];
-    int skip=solve(i+1,memo,nums,n);
-    int take=nums[i]+solve(i+2,memo,nums,n);
-     return memo[i]=max(skip,take);
-   }
+//tabulation 
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>memo(n,-1);
-        return solve(0,memo,nums,n);
+         if (n == 1) return nums[0];
+         vector<int>dp(n,0);
+         dp[0]=nums[0];
+         dp[1]=max(nums[0],nums[1]);
+         for(int i=2;i<n;i++){
+            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+         }
+         return dp[n-1];
     }
 };
