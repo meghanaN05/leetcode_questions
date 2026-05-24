86fb2bc200dfc2bf6f5863541b39753f396306fd
Meghana N <meghananoochila@gmail.com>
2026-05-24 12:24:19 +0530

    Time: 0 ms (100%), Space: 11.6 MB (84.07%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
-public:   
-    int unique(int i,int j,vector<vector<int>>& obstacleGrid,vector<vector<int>>&memo){
-         if(i<0 || j<0 ) return 0;
-        if(obstacleGrid[i][j]==1) return 0;
-        if(i==0 && j==0) return 1;
-       
-        
-        if(memo[i][j]!=-1) return memo[i][j];
-        return memo[i][j]=unique(i-1,j,obstacleGrid,memo)+unique(i,j-1,obstacleGrid,memo);
-    }
-
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m=obstacleGrid[0].size();
-        int n=obstacleGrid.size();
-        vector<vector<int>>memo(n,vector<int>(m,-1));
-        return unique(n-1,m-1,obstacleGrid,memo);
-
+public:
+//space optimized solution
+    int uniquePathsWithObstacles(vector<vector<int>>&grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<int>dp(m,0);
+        dp[0]=1;
+        if(grid[0][0]==1) return 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1) dp[j]=0;
+               else if(j>0){
+                    dp[j]=dp[j]+dp[j-1];
+                }
+            }
+        }
+        return  dp[m-1];
     }
 };
