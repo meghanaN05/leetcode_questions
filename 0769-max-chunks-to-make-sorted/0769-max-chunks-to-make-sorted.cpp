193bb44e59a3042b444dc8a28ffcbdd917455a76
Meghana N <meghananoochila@gmail.com>
2026-05-29 11:40:35 +0530

    Time: 0 ms (100%), Space: 9.3 MB (16.15%) - LeetHub

---
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int chunks = 0;
+        int maxi = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            maxi = max(maxi, arr[i]);
+            if (maxi == i) {
+                chunks++;
+            }
+        }
+        
+        return chunks;
+    }
+};
