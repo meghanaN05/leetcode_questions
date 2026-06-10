29b82d4f69dc0a7f0076bfc42ad3617e8f611f87
Meghana N <meghananoochila@gmail.com>
2026-06-11 00:12:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int curMax = 0, maxi = INT_MIN;
-        for(auto c : nums)
-            curMax = max(c, curMax + c),
-            maxi = max(maxi, curMax);
+        int maxi=0;
+        int curr=0;
+        for(int i=0;i<nums.size();i++){
+            curr=max(curr,curr+nums[i]);
+            maxi=max(maxi,curr);
+        }
         return maxi;
     }
 };
